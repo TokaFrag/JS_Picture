085a94aba78e799454f610f1e26a080c1a4d5f55
Tokmianin Alex <tokafrag@ya.ru>
2021-12-19 16:21:43 +0500

    Активирован слайдер и модалка с подарком

---
+++ src/js/modules/modals.js
@@ -0,0 +1,105 @@
+const modals = () => {
+    let btnPressed;
+
+    function bindModal(triggerSelector, modalSelector, closeSelector, destroy = false) {
+        const trigger = document.querySelectorAll(triggerSelector),
+            modal = document.querySelector(modalSelector),
+            close = document.querySelector(closeSelector),
+            windows = document.querySelectorAll('[data-modal]'),
+            scroll = calcScrol();
+
+
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if (e.target) {
+                    e.preventDefault();
+                }
+                btnPressed = true;
+
+                if (destroy) {
+                    item.remove();
+                }
+
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                    item.classList.add('animated', 'fadeIn');
+                });
+
+                modal.style.display = 'block';
+                document.body.style.overflow = "hidden";
+                document.body.style.marginRight = `${scroll}px`;
+            });
+        });
+
+        close.addEventListener('click', () => {
+            windows.forEach(item => {
+                item.style.display = 'none';
+
+            });
+
+            modal.style.display = "none";
+            document.body.style.overflow = "";
+            document.body.style.marginRight = `0px`;
+        });
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) {
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+                modal.style.display = "none";
+                document.body.style.overflow = "";
+                document.body.style.marginRight = `0px`;
+            }
+        });
+    }
+
+    function showModalByTime(selector, time) {
+        setTimeout(function () {
+            let display;
+
+            document.querySelectorAll('[data-modal]').forEach(item => {
+                if (getComputedStyle(item).display !== 'none') {
+                    display = "block";
+                }
+            });
+
+            if (!display) {
+                document.querySelector(selector).style.display = 'block';
+                document.body.style.overflow = "hidden";
+                let scroll = calcScrol();
+                document.body.style.marginRight = `${scroll}px`;
+            }
+        }, time);
+    }
+
+    function calcScrol() {
+        let div = document.createElement('div');
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+        return scrollWidth;
+    }
+
+    function openByScroll(selector) {
+        window.addEventListener('scroll', () => {
+            if (!btnPressed && (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight)) {
+                document.querySelector(selector).click();
+            }
+        });
+    }
+
+
+    bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
+    bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
+    bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true);
+    openByScroll('.fixed-gift');
+    // showModalByTime('.popup-consultation', 6000);
+};
+
+export default modals;
